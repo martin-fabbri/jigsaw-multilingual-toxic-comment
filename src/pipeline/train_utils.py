35b4c58165ac1b5d930b263be8277b2493ab9a14
Martin Fabbri <martin.fabbri.a@gmail.com>
2021-03-01 03:02:05 +0000

    adding MapDict to train utils

---
+++ src/pipeline/train_utils.py
@@ -0,0 +1,17 @@
+class MapDict(dict):
+    """todo: add doc."""
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError as k:
+            raise AttributeError(k)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, key):
+        try:
+            del self[key]
+        except KeyError as k:
+            raise AttributeError(k)
